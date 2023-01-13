a9db075de87d9f481181977219a8b654e48a5e3a
Denga02 <115780383+Denga02@users.noreply.github.com>
2023-01-13 21:45:34 +0100

    Add files via upload

---
+++ TsBot_starten.bat
@@ -0,0 +1,15 @@
+@echo off
+
+title TsBot starten
+
+echo Programm um den Ts3Bot laufen zu lassen
+echo Befehlzeile im Hintergrund offen lassen, damit es auch funktioniert
+echo mit strg + c das Prgramm abbrechen
+
+pause
+
+cd C:\java\bot_jar
+java -jar Ts3Bot.jar
+
+pause
+
