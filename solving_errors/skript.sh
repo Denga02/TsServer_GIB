b4fcba0af173d599215bda02b462c23f5f19ba9a
Denga02 <115780383+Denga02@users.noreply.github.com>
2023-10-25 13:34:27 +0200

    Add files via upload

---
+++ solving_errors/skript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+perform_action() {
+    case $message in
+        "Zufallszahl: 1")
+            echo "Aktion 1 ausführen"
+            ;;
+        "Zufallszahl: 0")
+            echo "Aktion 0 ausführen"
+            ;;
+        *)
+            echo "Unbekannte Meldung: $message"
+            ;;
+    esac
+}
+
+message="standard"
+
+x=1
+while [ true ]
+do
+  echo "Message: $x times"
+  x=$(( $x + 1 ))
+  message=$(./a.out)
+  perform_action $message
+  sleep 1
+done
